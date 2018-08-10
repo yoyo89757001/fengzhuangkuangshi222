c875c24a31efb35552246e53b49c668d113168d9
yoyo89757001 <332663557@qq.com>
2018-08-10 17:31:38 +0800

    Initial commit

---
+++ mylibrary/src/main/java/megvii/facepass/ruitong/Interstellar.java
@@ -0,0 +1,19 @@
+package megvii.facepass.ruitong;
+
+import android.util.Log;
+
+public class Interstellar {
+
+
+
+
+//    public static String guyour(String s){
+//        Log.d("Interstellar", "dddddddddddddddd"+s);
+//return "ddddddeee";
+//    }
+
+
+
+
+
+}
